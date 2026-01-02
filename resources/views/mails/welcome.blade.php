f60ac065f7f05a0a984240591250e20edea4a9ed
alex <alex@alexsykes.net>
2026-01-02 16:59:32 +0300

    Welcome mail added

---
+++ resources/views/mails/welcome.blade.php
@@ -0,0 +1,18 @@
+<x-automail>
+    <body class="">
+    <p>Dear {{$name}}, <br>Thank you for registering with SlopeFinder UK. I hope that you will find it both easy to use
+        and useful.
+    </p>
+    <p>As you may already know, the main purpose of SlopeFinder UK is to support the slope soaring community by
+        maintaining and sharing information about the many usable sites throughout the UK (and further afield). Please
+        be aware though, that as this data has been gathered over many years, it may be out of date and permission to
+        use some sites may have been withdrawn. <b>Please note that inclusion of a site here does not imply any right of
+            access or use. Permission must always be obtained from landowners.</b></p>
+    <p>In order to maintain and extend the site data, registered users are invited to contribute both by suggesting
+        updates - there is a link from each site's detail page - and by submitting new sites. I am therefore pleased to
+        let you know that your account has now been approved and you are able to add new sites to our
+        database.</p>
+    <p>Thanks, again, for registering. Please reply to this email if you have any questions or requests.<br>Alex -
+        SlopeFinder Admin</p>
+    </body>
+</x-automail>
